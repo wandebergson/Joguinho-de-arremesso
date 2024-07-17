60e4cbe332622015f9b06c4797d722e4e494ba9d
wandebergson <wandebergsonferreira@gmail.com>
2024-07-17 20:17:36 -0300

    function animate and fps  created

--- ts e js/game.js
+++ ts e js/game.js
@@ -1,7 +1,17 @@
 var canvas = document.querySelector('canvas');
+console.log(typeof (canvas));
 var ctx = canvas.getContext('2d');
 var canvasWidth = 800;
 var canvasHeight = 576;
-canvas.width = canvasWidth;
+canvas.widthidth = canvasWidth;
 canvas.height = canvasHeight;
-ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+var prevTime = 0;
+animate();
+function animate() {
+    window.requestAnimationFrame(animate);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    var delta = (performance.now() - prevTime / 1000);
+    var fps = 1 / delta;
+    prevTime = performance.now();
+    console.log("Fps: ".concat(fps));
+}
--- ts e js/game.ts
+++ ts e js/game.ts
@@ -1,10 +1,21 @@
 const canvas:any = document.querySelector('canvas');
+console.log(typeof(canvas));
 const ctx:any = canvas.getContext('2d');
 const canvasWidth: number = 800;
 const canvasHeight: number = 576;
 
-canvas.width = canvasWidth;
+canvas.widthidth = canvasWidth;
 canvas.height = canvasHeight;
 
-ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+let prevTime:number = 0;
+animate()
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    let delta: number = (performance.now() - prevTime / 1000);
+    let fps: number = 1 / delta;
+    prevTime = performance.now();
+    console.log(`Fps: ${fps}`)
+}
 
