9f37b3dde8418bbb28933da42e3efdda06d553b8
wandebergson <wandebergsonferreira@gmail.com>
2024-07-20 19:39:17 -0300

    create react app

--- ts e js/game.js
+++ ts e js/game.js
@@ -1,17 +1,18 @@
 var canvas = document.querySelector('canvas');
 console.log(typeof (canvas));
 var ctx = canvas.getContext('2d');
+console.log(typeof (ctx));
 var canvasWidth = 800;
 var canvasHeight = 576;
-canvas.widthidth = canvasWidth;
+canvas.width = canvasWidth;
 canvas.height = canvasHeight;
-var prevTime = 0;
 animate();
 function animate() {
+    var prevTime = 0;
     window.requestAnimationFrame(animate);
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     var delta = (performance.now() - prevTime / 1000);
     var fps = 1 / delta;
     prevTime = performance.now();
-    console.log("Fps: ".concat(fps));
+    //console.log("Fps: ".concat(fps));
 }
--- ts e js/game.ts
+++ ts e js/game.ts
@@ -1,16 +1,17 @@
-const canvas:any = document.querySelector('canvas');
+const canvas:any = document.querySelector('canvas'); 
 console.log(typeof(canvas));
 const ctx:any = canvas.getContext('2d');
+console.log(typeof(ctx));
 const canvasWidth: number = 800;
 const canvasHeight: number = 576;
 
-canvas.widthidth = canvasWidth;
+canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
-let prevTime:number = 0;
 animate()
 
 function animate(){
+    let prevTime:number = 0;
     window.requestAnimationFrame(animate);
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     let delta: number = (performance.now() - prevTime / 1000);
